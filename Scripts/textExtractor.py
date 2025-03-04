8622aa10606c5e2e61dc0cc1125f24ea8ce8d94f
haghrah <arslan.haghrah@gmail.com>
2025-03-04 15:55:02 +0330

    Some updates ...

--- Scripts/textExtractor.py
+++ Scripts/textExtractor.py
@@ -5,6 +5,22 @@ separator = "\n________________________________________________\n"
 
 digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
+la2arDict = {" ":"", "\n":"", "y":"", "û":"", "s":"", "u":"", "f":"", 
+             "h":"", "â":"", "c":"", "i":"", "̇":"", "b":"", "k":"", 
+             "t":"", "a":"", "d":"", "ğ":"", "l":"", "g":"", "m":"", 
+             "e":"", "n":"", "z":"", "ı":"", "r":"", "ç":"", "1":"", 
+             "©":"", ".":"", "ü":"", "v":"", "p":"", "3":"", "7":"", 
+             "4":"", "2":"", "0":"", "9":"", "8":"", "-":"", "5":"", 
+             "w":"", "o":"", ":":"", "@":"", "’":"", "ö":"", "ş":"", 
+             "(":"", "6":"", ")":"", "‘":"", "î":"", "[":"", "]":"", 
+             "?":"", "~":"", ",":"", "ñ":"", "é":"", ";":"", "ˆ":"", 
+             "<":"", ">":"", "j":"", "ô":"", "x":"", "/":"", "!":"", }
+
+def rawLa2Ar(token):
+    o = ""
+    
+    return o
+
 def filterWords(word):
     for character in word:
         if character in digits:
@@ -13,25 +29,39 @@ def filterWords(word):
 
 class WordExtractor:
 
-    suffixes = ["lar", 
-                "ler", 
-                "sız", 
-                "suz", 
-                "siz", 
-                "süz", 
-                "sa", 
-                "se", 
-                "mış", 
-                "muş", 
-                "miş", 
-                "müş", 
-                "ke", 
-                "din", 
-                "de", 
-                "ni", 
-                "lig", 
-                "ig", 
-                "li", ]
+    suffixes = ["lar", "ler", 
+                "sız", "suz", "siz", "süz", 
+                "sa", "se", 
+                "mış", "muş", "miş", "müş", 
+                "lığ", "lık", "luğ", "luk", "lig", "lik", "lüg", "lük", 
+                "lı", "lu", "li", "lü", 
+                "ıp", "up", "ip", "üp", 
+                "ma", "me", 
+                "ke", "ka", 
+                "dın", "din", "tın", "tin", 
+                "da", "de", "ta", "te", 
+                "nı", "nu", "ni", "nü", 
+                "maz", "mez", 
+                "mağ", "mak", "meg", "mek", 
+                "ığ", "uğ", "ig", "üg", 
+                "ğı", "ğu", "gi", "gü", 
+                "la", "le", 
+                "lan", "len", 
+                "çı", "çu", "çi", "çü", 
+                "ğınça", "ğunça", "ginçe", "günçe", 
+                "ğısı", "ğusı", "gisi", "güsi", 
+                "dım", "dum", "dim", "düm", 
+                "dıñ", "duñ", "diñ", "düñ", 
+                "dı", "du", "di", "dü", 
+                "dık", "duk", "dik", "dük", 
+                "dıñız", "duñuz", "diñiz", "düñüz", 
+                "dılar", "dular", "diler", "düler", 
+                "tı", "tu", "ti", "tü", 
+                "ça", "çe", 
+                "yın", "yin", 
+                "dır", "dur", "dir", "dür", 
+                "tır", "tur", "tir", "tür", 
+                "ğıl", "gil", ]
     reversedSuffixes = []
 
     def __init__(self, ):
@@ -40,26 +70,28 @@ class WordExtractor:
             self.reversedSuffixes.append(suffix[::-1])
     
     def etymonize(self, word):
-        minRootLen = 4
-        if len(word) <= minRootLen:
+        minEtumonLen = 3
+        if len(word) <= minEtumonLen:
             return word
         else:
             word = word[::-1]
-            while len(word) >= minRootLen:
-                for i in range(len(word) - minRootLen):
-                    if word[:i] in self.reversedSuffixes:
-                        word = word[i:]
-                        break
-                if i == len(word) - minRootLen - 1:
-                    break
-            word = word[::-1]
-            return word
+            index = 0
+            for i in range(len(word) - minEtumonLen + 1):
+                if word[:i] in self.reversedSuffixes:
+                    index = i
+            
+            if index > 0:
+                word = word[index:]
+                return self.etymonize(word[::-1])
+            else:
+                return word[::-1]
 
     @property
     def etumonsSet(self):
         etumons = set()
         for word in self.uniqueWordsList:
-            etumons.add(self.etymonize(word[0]))
+            etumon = self.etymonize(word[0])
+            etumons.add(etumon)
         return etumons
 
     @property
@@ -118,3 +150,30 @@ if __name__ == "__main__":
     outFile.close()
     wordExtractor.saveToFile("Words.txt")
     wordExtractor.saveEtumonsToFile("Etumons.txt")
+    
+    tmp = "la2arDict = {"
+    for character in charSet:
+        tmp += f"\"{character}\":\"\", "
+    tmp += "}"
+    print(tmp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
